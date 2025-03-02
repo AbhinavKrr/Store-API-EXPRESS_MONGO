d7fbcc628576bbf29f0bda9fc6d971dead870dd0
AbhinavKrr <abhinavkumarblog@gmail.com>
2025-03-02 11:07:52 +0530

    in process

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,17 +2,38 @@ const Product = require('../model/product');
 
 
 const getAllProductsStatic = async (req, res) => {
+    const search = 'ab';
     const products = await Product.find({
-        featured : 'true' //string converted to boolean type by mongoose then to json
-        // feature: true boolean 
+        // featured : 'true' //string converted to boolean type by mongoose then to json
+        // featured: true boolean 
+        // name: 'emperor bed'
+        name:{
+            $regex: search, $options: 'i'
+        }
     });
     res.status(200).json({msg:"Success", nbHits: products.length, data:products});
 }
 
 const getAllProducts = async (req, res) =>{
-    console.log(req.query);
-    const products = await Product.find(req.query);
-    res.status(200).json({msg:"Success", data:products});
+    // console.log(req.query);
+    const {featured, company, name} = req.query;
+    const queryObj = {};
+    
+    if(featured){
+        queryObj.featured = featured === 'true'? true : false
+    }
+
+    if(company){
+        queryObj.company = company
+    }
+
+    if(name){
+        queryObj.name = {$regex: name, $options: 'i'}
+        // $options : 'i' this is for mathing uppercase and lowercase
+    }
+
+    const products = await Product.find(queryObj);
+    res.status(200).json({msg:"Success",nbHits: products.length, data:products});
 }
 
 const createProduct = async (req, res) =>{
