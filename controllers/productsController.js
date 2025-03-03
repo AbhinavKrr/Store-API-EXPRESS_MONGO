69bd7b1507c07473ee40cb56df9801abe5d44da5
AbhinavKrr <abhinavkumarblog@gmail.com>
2025-03-03 13:10:52 +0530

    in process

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,7 +4,7 @@ const Product = require('../model/product');
 const getAllProductsStatic = async (req, res) => {
     // Select filter method returns documents with only those files which we pass + id
     // Sorts filter method returns documents in asd or desd order acording to fields that we provide
-    const products = await Product.find({}).select('name price');
+    const products = await Product.find({}).sort('name').select('name price').limit(10).skip(1);
 
     res.status(200).json({msg:"Success", nbHits: products.length, data:products});
 }
@@ -42,6 +42,12 @@ const getAllProducts = async (req, res) =>{
         result = result.select(fieldList);
     }
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+ 
     const products = await result;
     res.status(200).json({msg:"Success",nbHits: products.length, data:products});
 }
